3641156d3c2c48ebdbe0cb57a7d7d4d2d3fe8473
Saman Khosravi <8khosrav@informatik.uni-hamburg.de>
2023-12-04 15:52:56 +0100

    add backend: maven /w spring boot dependencies

---
+++ backend/src/test/java/dev/saman/playground/PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.saman.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
