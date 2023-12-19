29d40020554da22fab62c5e873b0335e7140a088
Saman Khosravi <8khosrav@informatik.uni-hamburg.de>
2023-12-19 01:04:21 +0100

    guarantee csrf cookie

--- src/main/java/dev/saman/playground/config/WebSecurityConfig.java
+++ src/main/java/dev/saman/playground/config/WebSecurityConfig.java
@@ -13,10 +13,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SimpleSavedRequest;
+
+import dev.saman.playground.web.CookieCsrfFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
@@ -31,14 +39,13 @@ public class WebSecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
-		requestHandler.setCsrfRequestAttributeName(null);
 		http.authorizeHttpRequests((authorize) -> authorize
 				.requestMatchers("/login", "/RegisterForms.html", "/LoginForms.html", "/api/users/*").permitAll()
 				.anyRequest().authenticated()).httpBasic(Customizer.withDefaults())
 				.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-						.csrfTokenRequestHandler(requestHandler))
-				.headers(headers -> headers.frameOptions(frameOptionsConfig -> frameOptionsConfig.sameOrigin()));
+						.csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()))
+				.headers(headers -> headers.frameOptions(frameOptionsConfig -> frameOptionsConfig.sameOrigin()))
+				.addFilterAfter(new CookieCsrfFilter(), BasicAuthenticationFilter.class);
 
 		return http.build();
 	}
@@ -50,6 +57,21 @@ public class WebSecurityConfig {
 		UserDetails user2 = User.builder().username("user").password(result).roles("USER").build();
 
 		return new InMemoryUserDetailsManager(user2);
+	}
+
+	@Bean
+	public RequestCache refererRequestCache() {
+		return new HttpSessionRequestCache() {
+			@Override
+			public void saveRequest(HttpServletRequest request, HttpServletResponse response) {
+				String referrer = request.getHeader("referrer");
+				if (referrer == null) {
+					referrer = request.getRequestURL().toString();
+				}
+				request.getSession().setAttribute("SPRING_SECURITY_SAVED_REQUEST", new SimpleSavedRequest(referrer));
+
+			}
+		};
 	}
 
 	@Bean
